9a9b4670cdfba68f6b6c12b45484cba9c00e66ba
Cecilia Wirén <cecilia.wiren@live.se>
2018-10-24 22:31:22 +0200

    td18 updates

---
+++ NDC/Calculator/AnOtherCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class AnOtherCalculator
+    {
+        public int Amount { get; set; }
+
+        public void Add(int i)
+        {
+            Amount = AddHelper.Add(Amount, i);
+        }
+
+
+        public void Subtract(int i)
+        {
+            Amount = AddHelper.Add(Amount, -1 * i);
+        }
+
+        public void LetsDivide(int i)
+        {
+            if (i < 0)
+                throw new ArgumentOutOfRangeException();
+            Amount /= i;
+        }
+    }
+}
